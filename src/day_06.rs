578b58f322be7bb64b21e1dd6e45310a7f1e2c09
AP Ljungquist <ap@ljungquist.eu>
2021-12-31 11:43:13 +0100

    Add d06 to the oracle

--- src/day_06.rs
+++ src/day_06.rs
@@ -1,18 +1,17 @@
 use std::collections::HashMap;
 use std::fs;
-
-fn _census(line: &str) -> HashMap<u32, u64> {
+type AnyError = Box<dyn std::error::Error>;
+type Census = HashMap<u32, u64>;
+fn _census(line: &str) -> Result<Census, AnyError> {
     let mut result = HashMap::new();
-    for countdown in line.trim_end().split(',').map(|s| {
-        s.parse::<u32>()
-            .unwrap_or_else(|_| panic!("Expected an int but got '{}'", s))
-    }) {
+    for countdown in line.trim_end().split(',') {
+        let countdown = countdown.parse::<u32>()?;
         *(result.entry(countdown).or_insert(0)) += 1;
     }
-    result
+    Ok(result)
 }
 
-fn _next_census(prev: &HashMap<u32, u64>) -> HashMap<u32, u64> {
+fn _next_census(prev: &Census) -> Census {
     let mut result = HashMap::with_capacity(prev.len());
     for (countdown, count) in prev {
         match countdown {
@@ -28,7 +27,7 @@ fn _next_census(prev: &HashMap<u32, u64>) -> HashMap<u32, u64> {
     result
 }
 
-fn _nth_census(initial: HashMap<u32, u64>, n: u32) -> HashMap<u32, u64> {
+fn _nth_census(initial: Census, n: u32) -> Census {
     let mut census = initial;
     for _ in 0..n {
         census = _next_census(&census);
@@ -36,21 +35,23 @@ fn _nth_census(initial: HashMap<u32, u64>, n: u32) -> HashMap<u32, u64> {
     census
 }
 
-pub fn part_1(input: &str) -> u64 {
-    let initial = _census(input);
-    _nth_census(initial, 80).values().sum()
+pub fn part_1(input: &str) -> Result<String, AnyError> {
+    let initial = _census(input)?;
+    let num_fish = _nth_census(initial, 80).values().sum::<u64>();
+    Ok(format!("{}", num_fish))
 }
 
-pub fn part_2(input: &str) -> u64 {
-    let initial = _census(input);
-    _nth_census(initial, 256).values().sum()
+pub fn part_2(input: &str) -> Result<String, AnyError> {
+    let initial = _census(input)?;
+    let num_fish = _nth_census(initial, 256).values().sum::<u64>();
+    Ok(format!("{}", num_fish))
 }
 
 fn _from_file<F, T>(func: F, stem: &str) -> T
 where
-    F: Fn(&str) -> T,
+    F: Fn(&str) -> Result<T, AnyError>,
 {
-    func(&fs::read_to_string(format!("inputs/06/{}.txt", stem)).unwrap())
+    func(&fs::read_to_string(format!("inputs/06/{}.txt", stem)).unwrap()).unwrap()
 }
 
 #[cfg(test)]
@@ -59,21 +60,21 @@ mod tests {
 
     #[test]
     fn part_1_works_on_example() {
-        assert_eq!(_from_file(part_1, "example"), 5934);
+        assert_eq!(_from_file(part_1, "example"), "5934");
     }
 
     #[test]
     fn part_1_works_on_input() {
-        assert_eq!(_from_file(part_1, "input"), 372300);
+        assert_eq!(_from_file(part_1, "input"), "372300");
     }
 
     #[test]
     fn part_2_works_on_example() {
-        assert_eq!(_from_file(part_2, "example"), 26984457539);
+        assert_eq!(_from_file(part_2, "example"), "26984457539");
     }
 
     #[test]
     fn part_2_works_on_input() {
-        assert_eq!(_from_file(part_2, "input"), 1675781200288);
+        assert_eq!(_from_file(part_2, "input"), "1675781200288");
     }
 }
