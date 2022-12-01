62c1dc13ec851bc2fdf3787bd4d570ac346e4f8c
AP Ljungquist <ap@ljungquist.eu>
2022-12-01 16:10:15 +0100

    Add option to save session cookies
    Also make it possible to override the user fingerprint value so that
    only one API call needs to be made when retrieving the input in the
    morning.
    
    Also separate input and answer scraping since the answer scraping
    cannot be cached, building the cache takes many requests and I don't
    want to spam the API unnecessarily.
    
    Multiple session cookies are supported to make it easier expanding the
    test data using multiple logins.
    
    Also format the answers as they will be formatted by the scraper to
    avoid diffs in the future when the scraper is used.

--- bin/scrape.py
+++ bin/scrape.py
@@ -8,6 +8,7 @@ import os
 import pathlib
 import re
 import time
+from typing import Optional
 
 import fire
 import requests
@@ -24,13 +25,15 @@ class Session:
     def __init__(
         self,
         session: str,
-        cache_location: pathlib.Path = pathlib.Path(__file__).parents[1] / "../../data",
+        cache_location: pathlib.Path = pathlib.Path(__file__).parents[1] / "data",
+        user_fingerprint: Optional[str] = None,
     ) -> None:
         self._cookie = session
         self._answers = {}
         self._cache_location = cache_location
         self._answers_location = cache_location / "answers.json"
         self._rate_limited_until = time.monotonic()
+        self._user_fingerprint = user_fingerprint
 
     def __enter__(self):
         with self._answers_location.open() as f:
@@ -63,9 +66,12 @@ class Session:
     def _question(self, year: int, day: int) -> str:
         return self._get_text(f"https://adventofcode.com/{year}/day/{day}")
 
-    @functools.cache
-    def _user_fingerprint(self) -> str:
-        return hashlib.sha1(self._input(2020, 2).encode("ascii")).hexdigest()[:10]
+    def user_fingerprint(self) -> str:
+        if self._user_fingerprint is None:
+            self._user_fingerprint = hashlib.sha1(
+                self._input(2020, 2).encode("ascii")
+            ).hexdigest()[:10]
+        return self._user_fingerprint
 
     def input(self, year: int, day: int) -> str:
         file_location = (
@@ -73,7 +79,7 @@ class Session:
             / "inputs"
             / f"{year:04}"
             / f"{day:02}"
-            / f"{self._user_fingerprint()}.txt"
+            / f"{self.user_fingerprint()}.txt"
         )
         if not file_location.exists():
             file_location.parent.mkdir(exist_ok=True, parents=True)
@@ -86,7 +92,7 @@ class Session:
             / "questions"
             / f"{year:04}"
             / f"{day:02}"
-            / f"{self._user_fingerprint()}.html"
+            / f"{self.user_fingerprint()}.html"
         )
         if not file_location.exists():
             file_location.parent.mkdir(exist_ok=True, parents=True)
@@ -107,36 +113,77 @@ class Session:
             raise AnswerNotFoundError from e
 
     def answer(self, year: int, day: int, part: int) -> str:
-        key = f"{year:04}/{day:02}/{part}/{self._user_fingerprint()}"
+        key = f"{year:04}/{day:02}/{part}/{self.user_fingerprint()}"
         if key not in self._answers:
             self._answers[key] = self._answer(year, day, part)
         return self._answers[key]
 
 
-def main(session_cookie: str) -> None:
-    """Scrape input-output pairs from the official website
-
-    :param session_cookie: The value of the session cookie stored by the browser after
-        authenticating to the the website.
-    """
-    with Session(session_cookie) as session:
-        for year in itertools.count(2015):
-            for day in range(1, 26):
+def _scrape_answers(session: Session) -> None:
+    logger.info(f"Scraping {session.user_fingerprint()} for answers")
+    for year in itertools.count(2015):
+        for day in range(1, 26):
+            for part in [1, 2]:
                 try:
-                    session.input(year, day)
+                    session.answer(year, day, part)
+                except AnswerNotFoundError:
+                    logger.debug("No answer for found %s/%s/%s", year, day, part)
                 except requests.HTTPError:
-                    logger.info(
-                        "Could not retrieve input for %s/%s/%s", year, day, part
+                    logger.debug(
+                        "Could not retrieve question for %s/%s/%s", year, day, part
                     )
                     return
 
-                for part in [1, 2]:
-                    try:
-                        session.answer(year, day, part)
-                    except AnswerNotFoundError:
-                        logger.debug("No answer for found %s/%s/%s", year, day, part)
+
+def _scrape_inputs(session: Session) -> None:
+    logger.info(f"Scraping {session.user_fingerprint()} for inputs")
+    for year in itertools.count(2015):
+        for day in range(1, 26):
+            try:
+                session.input(year, day)
+            except requests.HTTPError:
+                logger.info("Could not retrieve input for %s/%s", year, day)
+                return
+
+
+SAVED_COOKIES_PATH = pathlib.Path(__file__).with_suffix(".sessions.json")
+
+
+def _read_cookies() -> dict[str, str]:
+    with SAVED_COOKIES_PATH.open() as f:
+        return json.load(f)
+
+
+def _write_cookies(cookies: dict[str, str]) -> None:
+    with SAVED_COOKIES_PATH.open("w") as f:
+        json.dump(cookies, f, indent=4, sort_keys=True)
+
+
+class CLI:
+    @staticmethod
+    def add_session(session_cookie: str) -> None:
+        try:
+            cookies = _read_cookies()
+        except FileNotFoundError:
+            cookies = {}
+        with Session(session_cookie) as session:
+            user_fingerprint = session.user_fingerprint()
+        cookies[user_fingerprint] = session_cookie
+        _write_cookies(cookies)
+
+    @staticmethod
+    def scrape_answers() -> None:
+        for fingerprint, cookie in _read_cookies().items():
+            with Session(session=cookie, user_fingerprint=fingerprint) as session:
+                _scrape_answers(session)
+
+    @staticmethod
+    def scrape_inputs() -> None:
+        for fingerprint, cookie in _read_cookies().items():
+            with Session(session=cookie, user_fingerprint=fingerprint) as session:
+                _scrape_inputs(session)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=getattr(logging, os.environ.get("LEVEL", "WARNING")))
-    fire.Fire(main)
+    fire.Fire(CLI)
