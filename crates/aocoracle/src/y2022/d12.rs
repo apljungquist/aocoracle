7f2726934c937749845fbb7fa57095110a15aca0
AP Ljungquist <ap@ljungquist.eu>
2022-12-11 10:43:04 +0100

    Add scaffolding

---
+++ crates/aocoracle/src/y2022/d12.rs
@@ -0,0 +1,41 @@
+
+pub fn part_1(input: &str) -> anyhow::Result<usize> {
+    Ok(0)
+}
+
+pub fn part_2(input: &str) -> anyhow::Result<usize> {
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::testing::{assert_correct_answer_on_correct_input, assert_error_on_wrong_input};
+    use crate::Part;
+
+    use super::*;
+
+    #[test]
+    fn part_1_works_on_example() {
+        assert_correct_answer_on_correct_input!(part_1, "example", Part::One);
+    }
+
+    #[test]
+    fn part_1_works_on_input() {
+        assert_correct_answer_on_correct_input!(part_1, "6bb0c0bd67", Part::One);
+    }
+
+    #[test]
+    fn part_2_works_on_example() {
+        assert_correct_answer_on_correct_input!(part_2, "example", Part::Two);
+    }
+
+    #[test]
+    fn part_2_works_on_input() {
+        assert_correct_answer_on_correct_input!(part_2, "6bb0c0bd67", Part::Two);
+    }
+
+    #[test]
+    fn returns_error_on_wrong_input() {
+        assert_error_on_wrong_input!(part_1, part_2);
+    }
+}
