5c7d3bb14d6f30629fc3eaca5467c8c26db442f1
AP Ljungquist <ap@ljungquist.eu>
2022-12-12 06:33:16 +0100

    Add part 1

--- crates/aocoracle/src/y2022/d12.rs
+++ crates/aocoracle/src/y2022/d12.rs
@@ -1,6 +1,97 @@
+use hashbrown::HashMap;
+use pathfinding::prelude::bfs;
+use std::ops::{Add, AddAssign, Sub};
+
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn successors(&self, heights: &HashMap<Point, u8>) -> Vec<Point> {
+        let &Point { x, y } = self;
+        let height = heights[&self];
+        let mut result = vec![
+            Point { x: x, y: y - 1 },
+            Point { x: x, y: y + 1 },
+            Point { x: x - 1, y: y },
+            Point { x: x + 1, y: y },
+        ];
+        result.retain(|p| {
+            if let Some(h) = heights.get(p) {
+                *h < height + 2
+            } else {
+                false
+            }
+        });
+        result
+    }
+}
+
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+fn height_map(s: &str) -> (HashMap<(Point), u8>, Point, Point) {
+    let mut result = HashMap::new();
+    let mut start = Point::default();
+    let mut end = Point::default();
+    for (y, line) in s.lines().enumerate() {
+        for (x, height) in line.bytes().enumerate() {
+            match height {
+                b'S' => {
+                    let x = x as i32;
+                    let y = y as i32;
+                    let height = 0;
+                    start = Point { x, y };
+                    result.insert(Point { x, y }, height);
+                }
+                b'E' => {
+                    let x = x as i32;
+                    let y = y as i32;
+                    let height = b'z' - b'a';
+                    end = Point { x, y };
+                    result.insert(Point { x, y }, height);
+                }
+                _ => {
+                    let x = x as i32;
+                    let y = y as i32;
+                    let height = height - b'a';
+                    result.insert(Point { x, y }, height);
+                }
+            }
+        }
+    }
+    (result, start, end)
+}
 
 pub fn part_1(input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+    let (heights, start, end) = height_map(input);
+    let result = bfs(&start, |p| p.successors(&heights), |p| *p == end).unwrap();
+    // let mut distances = HashMap::with_capacity(heights.len());
+    // let mut curr_h = heights[&start];
+    // let mut curr_d = 0;
+    // distances.insert(curr_p, curr_d);
+
+    // while start != end {
+    //     for delta in deltas {
+    //         let next_p = curr_p + delta;
+    //         if let Some(next_h) = heights.get(&next_p){
+    //             if next_h == curr_h+1{
+    //                 di
+    //             }
+    //         }
+    //     }
+    // }
+    Ok(result.len() - 1)
 }
 
 pub fn part_2(input: &str) -> anyhow::Result<usize> {
