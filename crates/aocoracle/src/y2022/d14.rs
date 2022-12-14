5f4854f30fea0831421e1ab7eedde3a7025978e7
AP Ljungquist <ap@ljungquist.eu>
2022-12-14 06:32:30 +0100

    Add part 1

--- crates/aocoracle/src/y2022/d14.rs
+++ crates/aocoracle/src/y2022/d14.rs
@@ -1,5 +1,113 @@
+use hashbrown::HashSet;
+use std::ops::Add;
+
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+fn rock_paths(s: &str) -> Vec<Vec<Point>> {
+    let mut result = Vec::new();
+    for line in s.lines() {
+        result.push(
+            line.split(" -> ")
+                .map(|p| {
+                    let (x, y) = p.split_once(',').unwrap();
+                    let x = x.parse().unwrap();
+                    let y = y.parse().unwrap();
+                    Point { x, y }
+                })
+                .collect(),
+        );
+    }
+    result
+}
+
+fn rock_coordinates(paths: &Vec<Vec<Point>>) -> HashSet<Point> {
+    let mut result = HashSet::new();
+    for path in paths {
+        for j in 1..path.len() {
+            let i = j - 1;
+            let start = path[i];
+            let end = path[j];
+            if start.x == end.x {
+                let x = start.x;
+                for y in start.y.min(end.y)..=start.y.max(end.y) {
+                    result.insert(Point { x, y });
+                }
+            } else if start.y == end.y {
+                let y = start.y;
+                for x in start.x.min(end.x)..=start.x.max(end.x) {
+                    result.insert(Point { x, y });
+                }
+            } else {
+                panic!("Expected vertical or horizontal line")
+            }
+        }
+    }
+    result
+}
+
+fn moved_sand(blocked: &HashSet<Point>, curr: &Point) -> Option<Point> {
+    let movements = [
+        Point { x: 0, y: 1 },
+        Point { x: -1, y: 1 },
+        Point { x: 1, y: 1 },
+    ];
+    for movement in movements {
+        let next = *curr + movement;
+        if !blocked.contains(&next) {
+            return Some(next);
+        }
+    }
+    None
+}
+
+fn debug_print_coordinates(coordinates: &HashSet<Point>) {
+    let x_min = coordinates.iter().map(|p| p.x).min().unwrap();
+    let x_max = coordinates.iter().map(|p| p.x).max().unwrap();
+    let y_min = coordinates.iter().map(|p| p.y).min().unwrap();
+    let y_max = coordinates.iter().map(|p| p.y).max().unwrap();
+    for y in y_min..=y_max {
+        for x in x_min..=x_max {
+            if coordinates.contains(&Point { x, y }) {
+                print!("#");
+            } else {
+                print!(".")
+            }
+        }
+        println!();
+    }
+}
+
 pub fn part_1(input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+    let paths = rock_paths(input);
+    let mut coordinates = rock_coordinates(&paths);
+    let y_max = coordinates.iter().map(|p| p.y).max().unwrap();
+    debug_print_coordinates(&coordinates);
+    for i in 1.. {
+        let mut curr = Point { x: 500, y: 0 };
+        while let Some(next) = moved_sand(&coordinates, &curr) {
+            if next.y == y_max {
+                return Ok(i - 1);
+            }
+            curr = next;
+        }
+        coordinates.insert(curr);
+    }
+    unreachable!()
 }
 
 pub fn part_2(input: &str) -> anyhow::Result<usize> {
