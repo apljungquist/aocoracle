841a267bdde2267029d9a57625795464873c10c4
AP Ljungquist <ap@ljungquist.eu>
2022-12-21 06:21:03 +0100

    p1: Solved

--- crates/aocoracle/src/y2022/d21.rs
+++ crates/aocoracle/src/y2022/d21.rs
@@ -1,5 +1,68 @@
-pub fn part_1(input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+use hashbrown::HashMap;
+use std::str::FromStr;
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+enum Operation {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+impl Operation {
+    fn call(&self, lhs: u64, rhs: u64) -> u64 {
+        match self {
+            Self::Add => lhs + rhs,
+            Self::Sub => lhs - rhs,
+            Self::Mul => lhs * rhs,
+            Self::Div => lhs / rhs,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+enum Job {
+    Operand(u64),
+    Expression(String, Operation, String),
+}
+
+fn jobs(s: &str) -> anyhow::Result<HashMap<String, Job>> {
+    let mut result = HashMap::new();
+    for line in s.lines() {
+        let (id, job) = line.split_once(':').unwrap();
+        let id = id.to_string();
+        let job = match job.trim().parse() {
+            Ok(x) => Job::Operand(x),
+            Err(_) => {
+                let mut parts = job.trim().split_whitespace();
+                Job::Expression(
+                    parts.next().unwrap().to_string(),
+                    match parts.next().unwrap() {
+                        "+" => Operation::Add,
+                        "-" => Operation::Sub,
+                        "*" => Operation::Mul,
+                        "/" => Operation::Div,
+                        _ => panic!("Expected +-*/ but got"),
+                    },
+                    parts.next().unwrap().to_string(),
+                )
+            }
+        };
+        result.insert(id, job);
+    }
+    Ok(result)
+}
+
+fn evaluate(jobs: &HashMap<String, Job>, job: &String) -> u64 {
+    match jobs.get(job).unwrap() {
+        Job::Expression(lhs, op, rhs) => op.call(evaluate(jobs, &lhs), evaluate(jobs, &rhs)),
+        Job::Operand(x) => *x,
+    }
+}
+
+pub fn part_1(input: &str) -> anyhow::Result<u64> {
+    let jobs = jobs(input)?;
+    Ok(evaluate(&jobs, &"root".to_string()))
 }
 
 pub fn part_2(input: &str) -> anyhow::Result<i64> {
