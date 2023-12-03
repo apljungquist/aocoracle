2d1af454d13adb705de3cf8282e57a64622106b4
AP Ljungquist <ap@ljungquist.eu>
2023-12-03 06:35:35 +0100

    pt2

--- crates/aocoracle/src/y2023/d03.rs
+++ crates/aocoracle/src/y2023/d03.rs
@@ -69,7 +69,62 @@ pub fn part_1(input: &str) -> anyhow::Result<usize> {
 }
 
 pub fn part_2(input: &str) -> anyhow::Result<usize> {
+
+    let re = regex::Regex::new(&format!("([0-9]+|[^0-9])")).expect("Hard coded regex is valid");
+    ;
+
+    let mut next_id = 0;
+    let mut numbers = HashMap::new();
+    let mut symbols = HashMap::new();
+
+    for (row_num, line) in input.lines().enumerate() {
+        let row_num = row_num as isize;
+
+        let mut col_num: isize = 0;
+        for token in re.captures_iter(line) {
+            let token = token.get(1).unwrap().as_str();
+
+            if let Ok(number) = token.parse() {
+                for _ in 0..token.len() {
+                    numbers.insert((row_num, col_num), Number { id: next_id, value: number });
+                    col_num += 1;
+                }
+                next_id += 1;
+            } else if token != "." {
+                symbols.insert((row_num, col_num), token.to_string());
+                col_num += 1;
+            } else {
+                col_num += 1;
+            }
+        }
+    }
+
+
     let mut sum = 0;
+    for ((r, c), s) in symbols.into_iter() {
+        if s != "*" {
+            continue;
+        }
+        let mut used_numbers = HashSet::new();
+        let mut adjacent_numbers = Vec::new();
+        for dr in -1..=1 {
+            for dc in -1..=1 {
+                if dr == 0 && dc == 0 {
+                    continue;
+                }
+                let row = r + dr;
+                let col = c + dc;
+                if let Some(number) = numbers.get(&(row, col)) {
+                    if used_numbers.insert(number.id) {
+                        adjacent_numbers.push(number);
+                    }
+                }
+            }
+        }
+        if adjacent_numbers.len() == 2 {
+            sum += adjacent_numbers[0].value * adjacent_numbers[1].value
+        }
+    }
     Ok(sum)
 }
 
