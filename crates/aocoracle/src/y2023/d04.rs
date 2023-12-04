9c7189e9344f8972947d4accbf8435747dc4d450
AP Ljungquist <ap@ljungquist.eu>
2023-12-04 06:26:11 +0100

    pt2

--- crates/aocoracle/src/y2023/d04.rs
+++ crates/aocoracle/src/y2023/d04.rs
@@ -1,13 +1,30 @@
-use hashbrown::HashSet;
+use hashbrown::{HashMap, HashSet};
 
-pub fn part_1(input: &str) -> anyhow::Result<i32> {
-    let mut sum = 0;
-    for line in input.lines() {
+fn parsed_cards(input: &str) -> anyhow::Result<HashMap<usize, u32>> {
+    let mut cards = HashMap::with_capacity(input.lines().count());
+    for (line_num, line) in input.lines().enumerate() {
         let (answers, guesses) = line.split_once('|').unwrap();
         let (title, answers) = answers.trim().split_once(':').unwrap();
-        let answers: HashSet<u32> = answers.trim().split(' ').flat_map(|s|s.parse().ok()).collect();
-        let guesses: Vec<u32> = guesses.trim().split(' ').flat_map(|s|s.parse().ok()).collect();
+        let answers: HashSet<u32> = answers
+            .trim()
+            .split(' ')
+            .flat_map(|s| s.parse().ok())
+            .collect();
+        let guesses: Vec<u32> = guesses
+            .trim()
+            .split(' ')
+            .flat_map(|s| s.parse().ok())
+            .collect();
         let num_correct = guesses.iter().filter(|g| answers.contains(g)).count() as u32;
+        cards.insert(line_num, num_correct);
+    }
+    Ok(cards)
+}
+
+pub fn part_1(input: &str) -> anyhow::Result<i32> {
+    let cards = parsed_cards(input)?;
+    let mut sum = 0;
+    for num_correct in cards.values() {
         if let Some(p) = num_correct.checked_sub(1) {
             let value = 2_i32.pow(p);
             sum += value;
@@ -16,8 +33,25 @@ pub fn part_1(input: &str) -> anyhow::Result<i32> {
     Ok(sum)
 }
 
-pub fn part_2(input: &str) -> anyhow::Result<u32> {
+fn card_value(cards: &HashMap<usize, u32>, curr: usize) -> u32 {
+    let num_correct = *cards.get(&curr).unwrap();
+    let first = curr + 1;
+    let last = curr + num_correct as usize;
     let mut sum = 0;
+    for next in first..=last {
+        sum += card_value(cards, next);
+    }
+    sum + 1
+}
+
+pub fn part_2(input: &str) -> anyhow::Result<u32> {
+    let cards = parsed_cards(input)?;
+    let mut sum = 0;
+    for (card_num, num_correct) in cards.iter() {
+        let value = card_value(&cards, *card_num);
+        println!("{value}");
+        sum += value;
+    }
     Ok(sum)
 }
 
