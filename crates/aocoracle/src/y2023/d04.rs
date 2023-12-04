35b55c796ef6d95a232694115ba0b71c0c674df4
AP Ljungquist <ap@ljungquist.eu>
2023-12-04 06:13:57 +0100

    pt1

--- crates/aocoracle/src/y2023/d04.rs
+++ crates/aocoracle/src/y2023/d04.rs
@@ -1,5 +1,18 @@
-pub fn part_1(input: &str) -> anyhow::Result<u32> {
+use hashbrown::HashSet;
+
+pub fn part_1(input: &str) -> anyhow::Result<i32> {
     let mut sum = 0;
+    for line in input.lines() {
+        let (answers, guesses) = line.split_once('|').unwrap();
+        let (title, answers) = answers.trim().split_once(':').unwrap();
+        let answers: HashSet<u32> = answers.trim().split(' ').flat_map(|s|s.parse().ok()).collect();
+        let guesses: Vec<u32> = guesses.trim().split(' ').flat_map(|s|s.parse().ok()).collect();
+        let num_correct = guesses.iter().filter(|g| answers.contains(g)).count() as u32;
+        if let Some(p) = num_correct.checked_sub(1) {
+            let value = 2_i32.pow(p);
+            sum += value;
+        }
+    }
     Ok(sum)
 }
 
