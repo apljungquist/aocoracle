6e5ad95089d814c800b0de54472c786229474595
AP Ljungquist <ap@ljungquist.eu>
2023-12-04 06:58:15 +0100

    Tidy up
    Parse numbers on cards as u8 because they are at most 2 characters long.
    
    Store numbers in a vector because the keys are dense and this is faster
    (cuts time for pt2 from about 18ms to 11ms).

--- crates/aocoracle/src/y2023/d04.rs
+++ crates/aocoracle/src/y2023/d04.rs
@@ -1,55 +1,83 @@
+use anyhow::{bail, Context};
 use hashbrown::{HashMap, HashSet};
 
-fn parsed_cards(input: &str) -> anyhow::Result<HashMap<usize, u32>> {
-    let mut cards = HashMap::with_capacity(input.lines().count());
-    for (line_num, line) in input.lines().enumerate() {
-        let (answers, guesses) = line.split_once('|').unwrap();
-        let (title, answers) = answers.trim().split_once(':').unwrap();
-        let answers: HashSet<u32> = answers
+/// Return a vector of the number of correct answers for each card.
+///
+/// Note that the cards are 0-indexed from this point.
+fn parsed_cards(input: &str) -> anyhow::Result<Vec<u8>> {
+    let mut cards = Vec::with_capacity(input.lines().count());
+    for (i, line) in input.lines().enumerate() {
+        let (answers, guesses) = line
+            .split_once('|')
+            .ok_or_else(|| anyhow::anyhow!("Expected exactly one '|' but got {line:?}"))?;
+        let (title, answers) = answers
             .trim()
-            .split(' ')
+            .split_once(':')
+            .ok_or_else(|| anyhow::anyhow!("Expected exactly one ':' but got {answers:?}"))?;
+        let card_num = title.strip_prefix("Card ").ok_or_else(|| {
+            anyhow::anyhow!("Expected title to start with 'Card ' but got {title:?}")
+        })?;
+        let card_num: usize = card_num
+            .trim()
+            .parse()
+            .with_context(|| format!("{title:?}"))?;
+        if card_num != i + 1 {
+            bail!("Expected card number to be {i} but got {card_num}");
+        }
+        let answers: HashSet<usize> = answers
+            .split_whitespace()
             .flat_map(|s| s.parse().ok())
             .collect();
-        let guesses: Vec<u32> = guesses
-            .trim()
-            .split(' ')
+        let guesses: Vec<usize> = guesses
+            .split_whitespace()
             .flat_map(|s| s.parse().ok())
             .collect();
-        let num_correct = guesses.iter().filter(|g| answers.contains(g)).count() as u32;
-        cards.insert(line_num, num_correct);
+        let num_correct = guesses
+            .iter()
+            .filter(|g| answers.contains(g))
+            .count()
+            .try_into()?;
+        cards.push(num_correct);
     }
     Ok(cards)
 }
 
-pub fn part_1(input: &str) -> anyhow::Result<i32> {
+fn cards_count(cache: &mut HashMap<usize, usize>, cards: &[u8], curr: usize) -> usize {
+    let num_correct = *cards.get(curr).unwrap();
+    let first = curr + 1;
+    let last = curr + num_correct as usize;
+
+    let mut count = 1;
+    for next in first..=last {
+        if let Some(v) = cache.get(&next) {
+            count += v;
+        } else {
+            count += cards_count(cache, cards, next);
+        }
+    }
+    cache.insert(curr, count);
+    count
+}
+
+pub fn part_1(input: &str) -> anyhow::Result<u32> {
     let cards = parsed_cards(input)?;
+
     let mut sum = 0;
-    for num_correct in cards.values() {
+    for num_correct in cards {
         if let Some(p) = num_correct.checked_sub(1) {
-            let value = 2_i32.pow(p);
-            sum += value;
+            sum += 2_u32.pow(p.into());
         }
     }
     Ok(sum)
 }
 
-fn card_value(cards: &HashMap<usize, u32>, curr: usize) -> u32 {
-    let num_correct = *cards.get(&curr).unwrap();
-    let first = curr + 1;
-    let last = curr + num_correct as usize;
-    let mut sum = 0;
-    for next in first..=last {
-        sum += card_value(cards, next);
-    }
-    sum + 1
-}
-
-pub fn part_2(input: &str) -> anyhow::Result<u32> {
+pub fn part_2(input: &str) -> anyhow::Result<usize> {
+    let mut cache = HashMap::new();
     let cards = parsed_cards(input)?;
+
     let mut sum = 0;
-    for (card_num, num_correct) in cards.iter() {
-        let value = card_value(&cards, *card_num);
-        println!("{value}");
+    for card_num in 0..cards.len() {
+        let value = cards_count(&mut cache, &cards, card_num);
         sum += value;
     }
     Ok(sum)
@@ -69,7 +97,7 @@ mod tests {
 
     #[test]
     fn part_1_works_on_input() {
-        assert_correct_answer_on_correct_input!(part_1, "INPUT", Part::One);
+        assert_correct_answer_on_correct_input!(part_1, "a0bd9806241e64ed", Part::One);
     }
 
     #[test]
@@ -79,7 +107,7 @@ mod tests {
 
     #[test]
     fn part_2_works_on_input() {
-        assert_correct_answer_on_correct_input!(part_2, "INPUT", Part::Two);
+        assert_correct_answer_on_correct_input!(part_2, "a0bd9806241e64ed", Part::Two);
     }
 
     #[test]
