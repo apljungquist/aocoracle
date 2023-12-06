24044b21b432444b3dd2ffc2652574bc69a352b2
AP Ljungquist <ap@ljungquist.eu>
2023-12-06 18:31:50 +0100

    WIP: Tidy

--- crates/aocoracle/src/y2023/d06.rs
+++ crates/aocoracle/src/y2023/d06.rs
@@ -1,20 +1,41 @@
-fn sheet(input: &str) -> Vec<(i64, i64)> {
+use std::str::Lines;
+
+use anyhow::bail;
+
+fn take_suffix<'a>(lines: &'a mut Lines, expected_prefix: &'static str) -> anyhow::Result<&'a str> {
+    let Some(line) = lines.next() else {
+        bail!("Expected at least one more line")
+    };
+    let Some((actual_prefix, suffix)) = line.split_once(':') else {
+        bail!("Expected line containing ':' but got {line:?}");
+    };
+    if actual_prefix != expected_prefix {
+        bail!("Expected line starting with {expected_prefix:?} but got {line:?}")
+    }
+    Ok(suffix)
+}
+fn races(input: &str) -> anyhow::Result<Vec<(i64, i64)>> {
     let mut lines = input.lines();
-    let (time_title, times) = lines.next().unwrap().split_once(':').unwrap();
-    let (distance_title, distances) = lines.next().unwrap().split_once(':').unwrap();
-    times
-        .split_whitespace()
-        .zip(distances.split_whitespace())
-        .map(|(t, d)| (t.parse().unwrap(), d.parse().unwrap()))
-        .collect()
+    let mut times = Vec::new();
+    for time in take_suffix(&mut lines, "Time")?.split_whitespace() {
+        times.push(time.parse()?);
+    }
+    let mut distances = Vec::new();
+    for distance in take_suffix(&mut lines, "Distance")?.split_whitespace() {
+        distances.push(distance.parse()?)
+    }
+    Ok(times.into_iter().zip(distances).collect())
 }
 fn race(input: &str) -> anyhow::Result<(i64, i64)> {
     let mut lines = input.lines();
-    let (time_title, times) = lines.next().unwrap().split_once(':').unwrap();
-    let (distance_title, distances) = lines.next().unwrap().split_once(':').unwrap();
-    // concatenate the strings in times
-    let time = times.split_whitespace().collect::<String>().parse()?;
-    let distance = distances.split_whitespace().collect::<String>().parse()?;
+    let time = take_suffix(&mut lines, "Time")?
+        .split_whitespace()
+        .collect::<String>()
+        .parse()?;
+    let distance = take_suffix(&mut lines, "Distance")?
+        .split_whitespace()
+        .collect::<String>()
+        .parse()?;
     Ok((time, distance))
 }
 
@@ -22,16 +43,16 @@ fn num_victory(time: i64, distance: i64) -> i64 {
     let b = time as f64;
     let a = -1.0;
     let c = -(distance + 1) as f64;
-    let x1 = (-b + (b * b - 4.0 * a * c).sqrt()) / (2.0 * a);
-    let x2 = (-b - (b * b - 4.0 * a * c).sqrt()) / (2.0 * a);
-    dbg!(x1.max(x2)).floor() as i64 - dbg!(x1.min(x2)).ceil() as i64 + 1
+    let first = (-b + (b * b - 4.0 * a * c).sqrt()) / (2.0 * a);
+    let last = (-b - (b * b - 4.0 * a * c).sqrt()) / (2.0 * a);
+    last.floor() as i64 - first.ceil() as i64 + 1
 }
 
 pub fn part_1(input: &str) -> anyhow::Result<i64> {
-    let races = sheet(input);
+    let races = races(input)?;
     Ok(races
         .into_iter()
-        .map(|(t, d)| dbg!(num_victory(t, d)))
+        .map(|(t, d)| num_victory(t, d))
         .product::<i64>())
 }
 
@@ -54,7 +75,7 @@ mod tests {
 
     #[test]
     fn part_1_works_on_input() {
-        assert_correct_answer_on_correct_input!(part_1, "INPUT", Part::One);
+        assert_correct_answer_on_correct_input!(part_1, "f422fdccdd57e74e", Part::One);
     }
 
     #[test]
@@ -64,7 +85,7 @@ mod tests {
 
     #[test]
     fn part_2_works_on_input() {
-        assert_correct_answer_on_correct_input!(part_2, "INPUT", Part::Two);
+        assert_correct_answer_on_correct_input!(part_2, "f422fdccdd57e74e", Part::Two);
     }
 
     #[test]
