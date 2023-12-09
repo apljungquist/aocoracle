90221f3815b50708c7ede0745a6f07a6387e74c5
AP Ljungquist <ap@ljungquist.eu>
2023-12-09 13:29:14 +0100

    pt1

--- crates/aocoracle/src/y2023/d09.rs
+++ crates/aocoracle/src/y2023/d09.rs
@@ -1,5 +1,35 @@
-pub fn part_1(input: &str) -> anyhow::Result<usize> {
-    Ok(0)
+use itertools::Itertools;
+
+fn parsed_input(input: &str) -> anyhow::Result<Vec<Vec<i64>>> {
+    let mut result = vec![];
+    for line in input.lines() {
+        let mut numbers = vec![];
+        for number in line.split_whitespace() {
+            numbers.push(number.parse()?);
+        }
+        result.push(numbers);
+    }
+    Ok(result)
+}
+fn extrapolate(history: &[i64]) -> i64 {
+    let mut diffs = vec![history.to_vec()];
+    loop {
+        let prev = diffs.last().unwrap();
+        let curr: Vec<_> = prev.iter().tuple_windows().map(|(&l, &r)| r - l).collect();
+        if curr.iter().all(|&v| v == 0) {
+            break;
+        }
+        diffs.push(curr);
+    }
+    let mut step = 0;
+    while let Some(diff) = diffs.pop() {
+        step += diff.iter().last().unwrap();
+    }
+    step
+}
+pub fn part_1(input: &str) -> anyhow::Result<i64> {
+    let histories = parsed_input(input)?;
+    Ok(histories.iter().map(|h| extrapolate(h)).sum())
 }
 
 pub fn part_2(input: &str) -> anyhow::Result<usize> {
@@ -14,13 +44,23 @@ mod tests {
     use super::*;
 
     #[test]
+    fn extrapolate_works_on_first_degree_example() {
+        assert_eq!(extrapolate(&vec![0, 3, 6, 9, 12, 15]), 18);
+    }
+
+    #[test]
+    fn extrapolate_works_on_third_degree_example() {
+        assert_eq!(extrapolate(&vec![10, 13, 16, 21, 30, 45]), 68);
+    }
+
+    #[test]
     fn part_1_works_on_example() {
         assert_correct_answer_on_correct_input!(part_1, "EXAMPLE", Part::One);
     }
 
     #[test]
     fn part_1_works_on_input() {
-        assert_correct_answer_on_correct_input!(part_1, "00fd4b4ba9668e83", Part::One);
+        assert_correct_answer_on_correct_input!(part_1, "INPUT", Part::One);
         // > 253652923
     }
 
@@ -31,7 +71,7 @@ mod tests {
 
     #[test]
     fn part_2_works_on_input() {
-        assert_correct_answer_on_correct_input!(part_2, "00fd4b4ba9668e83", Part::Two);
+        assert_correct_answer_on_correct_input!(part_2, "INPUT", Part::Two);
     }
 
     #[test]
