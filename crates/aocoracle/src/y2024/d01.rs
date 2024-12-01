74b3c20e55bf4e1bc4d0b57c5b284571e2cb8a71
AP Ljungquist <ap@ljungquist.eu>
2024-12-01 06:07:01 +0100

    pt1

--- crates/aocoracle/src/y2024/d01.rs
+++ crates/aocoracle/src/y2024/d01.rs
@@ -1,5 +1,17 @@
-pub fn part_1(input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part_1(input: &str) -> anyhow::Result<u64> {
+    let mut lhs = Vec::new();
+    let mut rhs = Vec::new();
+    for line in input.lines() {
+        let mut numbers = line.split_whitespace();
+        let l = numbers.next().unwrap().parse::<i64>()?;
+        let r = numbers.next().unwrap().parse::<i64>()?;
+        lhs.push(l);
+        rhs.push(r);
+    }
+    lhs.sort();
+    rhs.sort();
+    let sum = lhs.into_iter().zip(rhs.into_iter()).map(|(l, r)| {l.abs_diff(r)}).sum();
+    Ok(sum)
 }
 
 pub fn part_2(input: &str) -> anyhow::Result<i64> {
