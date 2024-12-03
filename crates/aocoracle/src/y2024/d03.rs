866341e267480eed75a804a4de6320b3dcbd2146
AP Ljungquist <ap@ljungquist.eu>
2024-12-03 06:18:12 +0100

    d03: initial

--- crates/aocoracle/src/y2024/d03.rs
+++ crates/aocoracle/src/y2024/d03.rs
@@ -1,9 +1,37 @@
 pub fn part_1(input: &str) -> anyhow::Result<u64> {
-    Ok(0)
+    // let mut input = input.to_string();
+    let re = regex::Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut sum = 0;
+    for cap in re.captures_iter(input.trim()) {
+        let lhs:u64 = cap[1].parse()?;
+        let rhs:u64 = cap[2].parse()?;
+        sum += lhs * rhs;
+    }
+    Ok(sum)
 }
 
-pub fn part_2(input: &str) -> anyhow::Result<i64> {
-    Ok(0)
+pub fn part_2(input: &str) -> anyhow::Result<u64> {
+
+    // let mut input = input.to_string();
+    let mut active = true;
+    let re = regex::Regex::new(r"(mul|do|don't)\(((\d{1,3}),(\d{1,3}))?\)").unwrap();
+    let mut sum = 0;
+    for cap in re.captures_iter(input.trim()) {
+
+        match &cap[1] {
+            "do"=> active = true,
+            "don't"=> active = false,
+            "mul"=>{
+                if active {
+                    let lhs:u64 = cap[3].parse()?;
+                    let rhs:u64 = cap[4].parse()?;
+                    sum += lhs * rhs;
+                }
+            }
+            _=> panic!("{cap:?}")
+        }
+    }
+    Ok(sum)
 }
 
 #[cfg(test)]
